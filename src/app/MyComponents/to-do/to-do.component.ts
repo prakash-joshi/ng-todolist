b05c11caac6384959b30609d20ae395a4e4cdb13
Prakash Joshi <prakash.joshi167@hotmail.com>
2023-02-05 23:17:39 +0530

    Added NavBar and create list of todo from todo model class

--- src/app/MyComponents/to-do/to-do.component.ts
+++ src/app/MyComponents/to-do/to-do.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-to-do',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./to-do.component.css']
 })
 export class ToDoComponent {
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      {
+        srno: 1,
+        title: "This is title 1",
+        desc: "This is description 1",
+        isactive: true
+      },
+      {
+        srno: 2,
+        title: "This is title 2",
+        desc: "This is description ",
+        isactive: true
+      }
+    ]
+  }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-todolist';
+
+  constructor(){
+
+    setTimeout(() => {
+      this.title = "Angular - To Do List"
+    }, 1500);
+    
+  }
 }
